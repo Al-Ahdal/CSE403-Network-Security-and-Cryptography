eb338e1209c5eaaf8a46169e3e7b4b333f40141f
sshubhamk1-csprojects <41034479+sshubhamk1-csprojects@users.noreply.github.com>
2019-02-13 12:39:51 +0530

    reverse table updated

--- reverse_ip_table.c
+++ reverse_ip_table.c
@@ -4,17 +4,17 @@
 int main(void){
 	int arr[8][8];
 	int count=33;
-	for(int i=7;i>=0;i--)arr[0][i]=count++;
+	for(int i=7;i>=0;i--)arr[i][0]=count++;
 	count=1;
-	for(int i=7;i>=0;i--)arr[1][i]=count++;
-	for(int i=1;i<4;i++){
-		for(int j=0;j<8;j++){
-			arr[2*i][j]=arr[2*(i-1)][j]+8;
-			arr[2*i+1][j]=arr[2*(i-1)+1][j]+8;
+	for(int i=7;i>=0;i--)arr[i][1]=count++;
+	for(int j=1;j<4;j++){
+		for(int i=0;i<8;i++){
+			arr[i][2*j]=arr[i][2*(j-1)]+8;
+			arr[i][2*j+1]=arr[i][2*(j-1)+1]+8;
 		}
 	}
 	for(int i=0;i<8;i++){
-		for(int j=0;j<8;j++)printf("%d\t",arr[j][i]);
+		for(int j=0;j<8;j++)printf("%d\t",arr[i][j]);
 		printf("\n");
 	}
 	return 0;
